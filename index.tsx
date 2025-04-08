8dc8afd54bc1b0a5e2200186c9f60f7dc6c2f648
CP0825 <constantin@persaud.de>
2025-04-08 18:22:32 +0200

    Delete index.tsx

--- index.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ display: "flex", height: "100vh", alignItems: "center", justifyContent: "center", fontSize: "2rem" }}>
-      👋 Hello from Constantin!
-    </div>
-  );
-}
-
